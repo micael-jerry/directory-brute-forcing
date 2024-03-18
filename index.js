bb9f2ea3db5efafd75d455021066dfb41a262e41
Micael Jerry <hei.jerry.2@gmail.com>
2024-03-18 10:09:54 +0300

    fix: async

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ const baseUrl = getUrlInArgv(argv);
 const data = fileToArray(dirListPath, 'utf8');
 
 async function brutForcing(baseUrl, wordListArray) {
-	const validUrl = await testUrl(baseUrl, wordListArray);
+	let array_1 = wordListArray.slice(0, Math.ceil(wordListArray.length / 2));
+	let array_2 = wordListArray.slice(Math.ceil(wordListArray.length / 2));
+
+	const [validUrl_1, validUrl_2] = await Promise.all(
+		[testUrl(baseUrl, array_1), testUrl(baseUrl, array_2)]
+	)
+
+	const validUrl = validUrl_1.concat(validUrl_2);
 	console.log("==================================================================");
 	console.log(validUrl);
 }
